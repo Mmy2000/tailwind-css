d3d2ea6af00d40dac356b96c9e589fd6cac53971
Mmy2000 <yahianada005@gmail.com>
2024-05-01 23:33:14 +0300

    starting with header and creating dark mode

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,16 @@
 let toggleBtn = document.querySelector('.toggle')
-let theme = "light"
+let theme = localStorage.getItem('theme')
+if (theme == "dark") {
+    document.documentElement.classList.add('dark')
+}
 toggleBtn.addEventListener('change' , ()=>{
     if (theme == "light") {
         theme="dark"
         document.documentElement.classList.add('dark')
+        localStorage.setItem('theme' , theme)
     }else{
         theme="light"
         document.documentElement.classList.remove('dark')
+        localStorage.setItem('theme' , theme)
     }
 })
